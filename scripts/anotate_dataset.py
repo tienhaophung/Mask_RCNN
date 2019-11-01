3d06156a567bba01b92b9f743f71d3955d8d77b4
tienhaophung <tienhaophung@gmail.com>
2019-11-01 14:08:13 +0700

    Assert image

--- scripts/anotate_dataset.py
+++ scripts/anotate_dataset.py
@@ -1,9 +1,12 @@
 from mrcnn.config import Config
 from mrcnn.model import MaskRCNN
 from mrcnn.visualize import display_instances
+
 import cv2
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
+import json_tricks
+import os
 
 # define 81 classes that the coco model knowns about
 class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
@@ -54,42 +57,114 @@ def plot_bounding_box_with_image(img, boxes, class_ids, scores):
     
     plt.show()
 
-pretrained_weights_dir = "./mask_rcnn_coco.h5"
-img_dir = "/Users/haophung/Documents/Mask_RCNN/images/mercedes_benz.jpg" # "/Users/haophung/Documents/Mask_RCNN/images/12283150_12d37e6389_z.jpg"
+def main():
+    pretrained_weights_dir = "./mask_rcnn_coco.h5"
+    data_dir = "/Users/haophung/OneDrive - VNU-HCMUS/car_dataset"
+    conf_threshold = 0.8 # Confidence threshold
+    area_threshold = 0.4 # Area threshold for small objq
+    anot_data = {}
 
-# Define model to inference, model_dir is directory to write logs
-rcnn = MaskRCNN(mode='inference', model_dir='./', config=InferenceConfig())
+    # Define model to inference, model_dir is directory to write logs
+    rcnn = MaskRCNN(mode='inference', model_dir='./', config=InferenceConfig())
 
-# Load pretrained weights on coco
-rcnn.load_weights(pretrained_weights_dir, by_name=True)
+    # Load pretrained weights on coco
+    rcnn.load_weights(pretrained_weights_dir, by_name=True)
 
-# load image
-img = cv2.imread(img_dir)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Fit model
-"""res is list of dictionary [dict]. Len of res is the number of images that you fit into
+    image_paths = {}
+    for directory in os.listdir(data_dir):
+        # Pass if this is a file
+        if "." in directory:
+            continue
+        image_paths[directory] = os.listdir(os.path.join(data_dir, directory))
+    # print(json_tricks.dumps(image_paths, indent=4))
 
-Where dict contain these keys:
-    "rois": the bounding boxes or ROI for detected objects (contain lower left and higher right points)
-    "masks": The masks for detected objects
-    "class_ids": The class index
-    "scores": The confidence level of predicted class
-"""
+    car_ids = [class_names.index('car'), class_names.index('truck')]
 
-res_list = rcnn.detect([img], verbose=0)
+    for car_model, imgnames in image_paths.items():
+        for imgname in imgnames:
+            if imgname.startswith(".") or imgname.startswith("Icon"):
+                continue
+            # Get img_dir
+            img_dir = os.path.join(data_dir, car_model, imgname)
+            
+            # load image
+            img = cv2.imread(img_dir)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            assert(img is not None)
+            # Fit model
+            """res is list of dictionary [dict]. Len of res is the number of images that you fit into
 
-res = res_list[0]
+            Where dict contain these keys:
+                "rois": the bounding boxes or ROI for detected objects (contain lower left and higher right points)
+                "masks": The masks for detected objects
+                "class_ids": The class index
+                "scores": The confidence level of predicted class
+            """
 
-# Get boxes for image
-boxes = res['rois']
-class_ids = res['class_ids']
-scores = res['scores']
+            res_list = rcnn.detect([img], verbose=0)
 
-# Filter out small bounding boxes
-if width * height < 7000:
-    continue
+            res = res_list[0]
 
-# Plot image with bounding boxes
-plot_bounding_box_with_image(img, boxes, class_ids, scores)
+            # Get boxes for image
+            boxes = res['rois']
+            class_ids = res['class_ids']
+            scores = res['scores']
 
+            anot_key = os.path.join(car_model, imgname)
+            anot_data[anot_key] = {"boxes":[], "scores":[], "scaled_area":[]}
+
+            fig = plt.figure()
+            ax = fig.gca()
+            ax.imshow(img)
+
+            img_height, img_width = img.shape[:2]
+
+            for i, box in enumerate(boxes):
+                if class_ids[i] not in car_ids or scores[i] < conf_threshold:
+                    continue
+                # lower left
+                y1, x1, y2, x2 = box
+                # Compute width and height
+                width, height = x2 - x1, y2 - y1
+
+                # Filter out small bounding boxes
+                scaled_area = (width * height) / (img_width * img_height)
+                if scaled_area < area_threshold:
+                    continue
+                
+                # Create rectangle
+                rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+                # Draw rect
+                ax.add_patch(rect)
+
+                # Write confidence
+                ax.text(x1, y1, "{:.2f}".format(scores[i]), fontsize=12, color='red')
+
+                # Save "scaled_area", "boxes" and "scores"
+                anot_data[anot_key]["scaled_area"].append(scaled_area)
+                anot_data[anot_key]["boxes"].append(box)
+                anot_data[anot_key]["scores"].append(scores[i])
+                
+            # # Plot image with bounding boxes
+            # plot_bounding_box_with_image(img, boxes, class_ids, scores)
+            break
+        break
+
+    plt.show()
+
+    # Save anot_data
+    with open(os.path.join(data_dir, "anotations.json"), "w") as json_file:
+        json_tricks.dump(anot_data, json_file, indent=4)
+
+def ParseArgs():
+    parser = argparse.ArgumentParser(description="Anotate dataset using Mask RCNN")
+    parser.add_argument("-ddir", "--data_dir", type=str, required=True,\
+        help="Data directory")
+    parser.add_argument("-wdir", "--weights_dir", type=str, default="./mask_rcnn_coco.h5", help="Path to pretrained weights of Mask RCNN")
+    parser.add_argument("-mdir", "--model_dir", type=str, default="./", help="Directory to write logs and save files")
+    parser.add_argument("-cf_thr", "--conf_threshold", type=float, default=0.8)
+    return parser.parse_args()
+
+def __name__ == "__main__":
+    main(ParseArgs())
--- scripts/prediction.py
+++ scripts/prediction.py
@@ -4,6 +4,7 @@ from mrcnn.visualize import display_instances
 import cv2
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
+import argparse
 
 # define 81 classes that the coco model knowns about
 class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
@@ -31,19 +32,26 @@ class InferenceConfig(Config):
     NUM_CLASSES = 1 + 80
 
 def plot_bounding_box_with_image(img, boxes, class_ids, scores):
-    ax = plt.gca() # get current axis
+    fig = plt.figure()
+    ax = fig.gca() # get current axis
     # ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     ax.imshow(img)
     ax.axis("off")
+    
+    car_ids = [class_names.index('car'), class_names.index('truck')]
 
+
+    img_height, img_width = img.shape[:2]
     for i, box in enumerate(boxes):
-        if class_ids[i] != class_names.index('car'):
+        if class_ids[i] not in car_ids:
             continue
         # lower left
         y1, x1, y2, x2 = box
         # Compute width and height
         width, height = x2 - x1, y2 - y1
         
+        print("Area: {}".format((width*height)/(img_width*img_height)))
+
         # Create rectangle
         rect = Rectangle((x1, y1), width, height, fill=False, color='red')
         # Draw rect
@@ -54,42 +62,57 @@ def plot_bounding_box_with_image(img, boxes, class_ids, scores):
     
     plt.show()
 
-pretrained_weights_dir = "./mask_rcnn_coco.h5"
-img_dir = "/Users/haophung/Documents/Mask_RCNN/images/mercedes_benz.jpg" # "/Users/haophung/Documents/Mask_RCNN/images/12283150_12d37e6389_z.jpg"
+def ParseArgs():
+    parser = argparse.ArgumentParser(description="Image prediction using Mask RCNN")
+    parser.add_argument("-idir", "--img_dir", type=str, required=True,\
+        help="Path to input image")
+    parser.add_argument("-wdir", "--weights_dir", type=str, default="./mask_rcnn_coco.h5", help="Path to pretrained weights of Mask RCNN")
+    parser.add_argument("-mdir", "--model_dir", type=str, default="./", help="Directory to write logs and save files")
+    
+    return parser.parse_args()
 
-# Define model to inference, model_dir is directory to write logs
-rcnn = MaskRCNN(mode='inference', model_dir='./', config=InferenceConfig())
+def main(args):
+    pretrained_weights_dir = args.weights_dir
+    img_dir = args.img_dir # "/Users/haophung/Documents/Mask_RCNN/images/mercedes_benz.jpg" # "/Users/haophung/Documents/Mask_RCNN/images/12283150_12d37e6389_z.jpg"
 
-# Load pretrained weights on coco
-rcnn.load_weights(pretrained_weights_dir, by_name=True)
+    # Define model to inference, model_dir is directory to write logs
+    rcnn = MaskRCNN(mode='inference', model_dir=args.model_dir, config=InferenceConfig())
 
-# load image
-img = cv2.imread(img_dir)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Load pretrained weights on coco
+    rcnn.load_weights(pretrained_weights_dir, by_name=True)
 
-# Fit model
-"""res is list of dictionary [dict]. Len of res is the number of images that you fit into
+    # load image
+    img = cv2.imread(img_dir)
+    assert(img is not None)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-Where dict contain these keys:
-    "rois": the bounding boxes or ROI for detected objects (contain lower left and higher right points)
-    "masks": The masks for detected objects
-    "class_ids": The class index
-    "scores": The confidence level of predicted class
-"""
+    # Fit model
+    """res is list of dictionary [dict]. Len of res is the number of images that you fit into
 
-res_list = rcnn.detect([img], verbose=0)
+    Where dict contain these keys:
+        "rois": the bounding boxes or ROI for detected objects (contain lower left and higher right points)
+        "masks": The masks for detected objects
+        "class_ids": The class index
+        "scores": The confidence level of predicted class
+    """
 
-res = res_list[0]
+    res_list = rcnn.detect([img], verbose=0)
 
-# Get boxes for image
-boxes = res['rois']
-class_ids = res['class_ids']
-scores = res['scores']
+    res = res_list[0]
 
-# Plot image with bounding boxes
-plot_bounding_box_with_image(img, boxes, class_ids, scores)
+    # Get boxes for image
+    boxes = res['rois']
+    class_ids = res['class_ids']
+    scores = res['scores']
 
-display_instances(img, res['rois'], res['masks'], res['class_ids'], class_names, res['scores'])
+    # Plot image with bounding boxes
+    plot_bounding_box_with_image(img, boxes, class_ids, scores)
+
+    # plt.figure()
+    display_instances(img, res['rois'], res['masks'], res['class_ids'], class_names, res['scores'])
+
+if __name__ == "__main__":
+    main(ParseArgs())
 
 
 
